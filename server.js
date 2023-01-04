9c82cc2735f806b0dc32bd3bf82efc2b76c0f68a
Tony <bowen.anth@gmail.com>
2023-01-03 17:56:45 -0700

    removed dummy db and finished signin route

--- server.js
+++ server.js
@@ -21,27 +21,6 @@ db.select('*').from('users').then(data => {
 
 const app = express();
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'Sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -50,20 +29,22 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    bcrypt.compare("apples", '$2a$10$0vHhuKuwaK3LDLivSAESjOrXmwDgozOurkA7j6eq0CeU.HQX545rK', 
-    function(err, res) {
-        console.log('first guess', res);
-    });
-    bcrypt.compare("veggies", '$2a$10$0vHhuKuwaK3LDLivSAESjOrXmwDgozOurkA7j6eq0CeU.HQX545rK', 
-    function(err,res) {
-        console.log('second guess', res);
-    });
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('Unable to get user'))
         } else {
-            res.status(400).json('error logging in');
+            res.status(400).json('Wrong credentials')
         }
+    })
+    .catch(err => res.status(400).json('Wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
