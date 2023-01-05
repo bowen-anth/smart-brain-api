71e8584d1217725b67f6b05baed1d4394deaf724
Tony <bowen.anth@gmail.com>
2023-01-05 10:14:17 -0700

    added env and git ignore file

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3001;
 
 const db = knex({
     client: 'pg',
     connection: {
         host : '127.0.0.1',
         user : 'tony',
-        port: 3000,
-        password : '',
+        // port: 3000,
+        password : process.env.SQL_PASSWORD,
         database : 'smart-brain'
     }
 });
@@ -30,20 +32,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {res.send('success')})
-app.post('/signin', signin.handleSignin(db, bcrypt)(req, res))
+app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+// app.listen(port, () => {
+//     console.log('app running on port ${process.env.PORT');
+// })
 app.listen(3001, () => {
-    console.log('app running on port 3001');
+    console.log(`app running on port ${port}`);
 })
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-
-*/
