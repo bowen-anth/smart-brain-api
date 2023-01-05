62a205a48b000579abf326a6020c35f96a47e9b6
Tony <bowen.anth@gmail.com>
2023-01-04 17:46:01 -0700

    moved clarifai to backend

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '8579fa0fc3014eb4bb093eadd0f1978b'
+  });
+
+ const handleApiCall = (req, res) => { 
+app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        console.log(entries)
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
