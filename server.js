1813a97429e5b110a97428bee2c3f52112d9e72b
Tony <bowen.anth@gmail.com>
2023-01-06 08:29:33 -0700

    comment out app get

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString : process.env.DATABASE_URL,
-        ssl: true,
+    ssl: {
+        rejectUnauthorized: false
+    }
     }
 });
 
@@ -28,7 +30,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('success')})
+// app.get('/', (req, res) => {res.send('success')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
