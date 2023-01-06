0b0d1f0e39cb1de095a76e05c73340ee34548f29
Tony <bowen.anth@gmail.com>
2023-01-06 08:34:26 -0700

    andreis register file

--- controllers/register.js
+++ controllers/register.js
@@ -1,35 +1,73 @@
 const handleRegister = (req, res, db, bcrypt) => {
     const { email, name, password } = req.body;
     if (!email || !name || !password) {
-        return res.status(400).json('Incorrect form submission');
+      return res.status(400).json('incorrect form submission');
     }
     const hash = bcrypt.hashSync(password);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0]);
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
+      db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
         })
-    .catch(err => res.status(400).json('unable to register'))
-    console.log(err)
-}
-
-module.exports = {
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+              // loginEmail[0] --> this used to return the email
+              // TO
+              // loginEmail[0].email --> this now returns the email
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      })
+      .catch(err => res.status(400).json('unable to register'))
+  }
+  
+  module.exports = {
     handleRegister: handleRegister
-};
+  };
+
+// const handleRegister = (req, res, db, bcrypt) => {
+//     const { email, name, password } = req.body;
+//     if (!email || !name || !password) {
+//         return res.status(400).json('Incorrect form submission');
+//     }
+//     const hash = bcrypt.hashSync(password);
+//         db.transaction(trx => {
+//             trx.insert({
+//                 hash: hash,
+//                 email: email
+//             })
+//             .into('login')
+//             .returning('email')
+//             .then(loginEmail => {
+//                 return trx('users')
+//                 .returning('*')
+//                 .insert({
+//                     email: loginEmail[0].email,
+//                     name: name,
+//                     joined: new Date()
+//                 })
+//                 .then(user => {
+//                     res.json(user[0]);
+//                 })
+//             })
+//             .then(trx.commit)
+//             .catch(trx.rollback)
+//         })
+//     .catch(err => res.status(400).json('unable to register'))
+// }
+
+// module.exports = {
+//     handleRegister: handleRegister
+// };
